950406235bf1057bf1a39680d388aaee2d219f59
Bart <barteqczech@gmail.com>
2023-08-25 07:28:30 +0200

    Some fun with good ol' CSS

---
+++ src/components/MenuItems.jsx
@@ -0,0 +1,26 @@
+export const MenuItems = [
+  {
+    title: "Główna",
+    url: "/",
+    cName: "nav-links",
+    icon: "fa-solid fa-house",
+  },
+  {
+    title: "O mnie",
+    url: "/o-mnie",
+    cName: "nav-links",
+    icon: "fa-solid fa-circle-info",
+  },
+  {
+    title: "Lekcje",
+    url: "/lekcje",
+    cName: "nav-links",
+    icon: "fa-solid fa-briefcase",
+  },
+  {
+    title: "Kontakt",
+    url: "/kontakt",
+    cName: "nav-links",
+    icon: "fa-solid fa-address-book",
+  },
+];
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,25 @@
 import { Component } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component {
-    render(){
-        return(
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">Logo</h1>
-                <ul className="nav-menu">
-                    <li>
-                        <a href="/"><i className="fa-solid fa-house"></i>Home</a>
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">Logo</h1>
+        <ul className="nav-menu">
+          {MenuItems.map((item, index) => (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                <i className={item.icon}></i>{item.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
