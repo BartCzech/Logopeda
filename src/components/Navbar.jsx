81986cc30c8fe32c6d92ca4e44d468cabf49dffa
Bart <barteqczech@gmail.com>
2023-08-26 08:49:05 +0200

    Navbar responsive for phones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,25 @@ import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({clicked: !this.state.clicked});
+  }
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Logo</h1>
-        <ul className="nav-menu">
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
           {MenuItems.map((item, index) => (
             <li key={index}>
               <a className={item.cName} href={item.url}>
-                <i className={item.icon}></i>{item.title}
+                <i className={item.icon}></i>
+                {item.title}
               </a>
             </li>
           ))}
