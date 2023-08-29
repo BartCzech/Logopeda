b33de1be1f7ade3383a9e5685ed91f2f28c82f5a
Bart <barteqczech@gmail.com>
2023-08-29 10:17:26 +0200

    Offer updated

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -11,14 +11,14 @@ const Offer = () => {
       <p>Możesz ze mną odbywać następujące zajęcia:</p>
       <div className="offercard">
         <OfferData image={therapy2Image}
-        heading="Bezpłatna Konsultacja Wstępna"
+        heading="Diagnoza logopedyczna"
         text="Nasza bezpłatna konsultacja wstępna ma na celu zrozumienie Twoich potrzeb i celów. W trakcie tej sesji omówimy Twoje wyzwania, odpowiemy na wszelkie pytania i przedstawimy spersonalizowany plan na Twoją podróż terapii mowy. Pracujmy razem, aby odblokować Twój pełny potencjał w komunikacji!" />
         <OfferData image={therapy3Image}
-        heading="Efektywna Komunikacja"
+        heading="Terapia logopedyczna"
         text="Ten rodzaj lekcji został stworzony, aby pomóc Ci zdobyć umiejętności i pewność siebie potrzebne do efektywnej komunikacji w różnych sytuacjach. Ten kompleksowy pakiet obejmuje spersonalizowane sesje indywidualne, dostosowane ćwiczenia i praktyczne strategie. Niezależnie czy chcesz doskonalić sztukę publicznego przemawiania, relacji społecznych czy komunikacji zawodowej, jesteśmy tutaj, aby Cię prowadzić na każdym etapie." />
-        <OfferData image={therapy4Image}
-        heading="Zestaw Rozwoju Mowy dla Dzieci"
-        text="Zestaw Rozwoju Mowy dla Dzieci – holistyczne podejście do wspierania umiejętności klarownej komunikacji u dzieci. Ten zestaw jest idealny dla rodziców, którzy chcą radzić sobie z opóźnieniami mowy, trudnościami w artykulacji lub rozwoju językowego u swoich maluchów. Nasze sesje prowadzone przez ekspertów są angażujące, oparte na zabawie i zaprojektowane tak, aby nauka była przyjemnością. Zobacz, jak pewność Twojego dziecka rośnie wraz z umiejętnościami komunikacyjnymi!" />
+        {/* <OfferData image={therapy4Image}
+        heading="Kinezjotaping"
+        text="" /> */}
       </div>
     </div>
   );
