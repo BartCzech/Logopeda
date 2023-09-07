aee6a53ec15113eca36b81e3302c0fde32bea019
Bart <barteqczech@gmail.com>
2023-09-07 18:32:01 +0200

    mail

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -4,7 +4,7 @@ const Action = (props) => {
   return (
     <div className="main">
       <h2>{props.first} <a href="tel:600377044">600377044</a></h2>
-      <h2>{props.second} <a href="biuro@logopeda-czechowice.pl">biuro@logopeda-czechowice.pl</a></h2>
+      <h2>{props.second} <a href="mailto:biuro@logopeda-czechowice.pl">biuro@logopeda-czechowice.pl</a></h2>
       <h2>{props.third}</h2>
     </div>
   )
