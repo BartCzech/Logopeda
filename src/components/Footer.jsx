42021eeb26ce0e55fe4f7cb14fe157210c18b64a
Bart <barteqczech@gmail.com>
2023-09-08 10:57:47 +0200

    horizontal again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,21 +4,19 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
+
       <div className="top">
         <div>
           <h1>Logopeda Wioletta Czech</h1>
         </div>
-        {/* <div>
-          <a href="/">
-            <i className="fa-brands fa-facebook-square"></i>
-          </a>
-        </div> */}
       </div>
+
       <div className="bottom">
         <div>
           <p>ul. Chabrowa 4A 43-502 Czechowice-Dziedzice</p>
           <p> <a href="tel:600377044">600 377 044</a> | <a href="mailto:biuro@logopeda-czechowice.pl">biuro@logopeda-czechowice.pl</a></p>
         </div>
+        
         <div className="row-links">
           <a href="/">
             <Link to="/">Główna</Link>
@@ -34,6 +32,7 @@ const Footer = () => {
           </a>
         </div>
       </div>
+
       <div className="unterschrift">
         <p>Copyright Bartłomiej Czech 2023</p>
       </div>
